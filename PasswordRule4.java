c59d816316696af02be993df2cbd62d55921047e
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-12 22:16:02 +0530

    Added: Rule 4 for password that is required at least 1 special character.

--- PasswordRule4.java
+++ PasswordRule4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PasswordRule3 {
+public class PasswordRule4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
@@ -12,10 +12,11 @@ public class PasswordRule3 {
         String password = scan.next();
 
         validatePassword(password);
+
     }
 
     private static void validatePassword(String password) {
-        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$";
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@+.-]).{8,20}$";
         Pattern pattern = Pattern.compile(passwordRegex);
         Matcher matcher = pattern.matcher(password);
         System.out.println("is valid password: " +matcher.matches());
